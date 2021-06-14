3e5bb8a2524cf1a1198388dc981d4d4a85553978
Gaev Cenne <gaev@evilparadise.com>
2021-06-14 13:47:47 +0200

    update: exercise 3

--- j-01.js
+++ j-01.js
@@ -4,5 +4,11 @@ console.log("Hello World !");
 */
 
 // 02. String
+/*
 var test ="My name is Andy";
 console.log(test)
+*/
+
+// 03. Concatenation
+var myName = "Andy Bouchet"
+console.log("Nice to meet you " + myName)
