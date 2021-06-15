b1af1dab641c559cd28769f95787badd6e63c63d
Gaev Cenne <gaev@evilparadise.com>
2021-06-15 14:12:35 +0200

    update: exercice 4

--- j-02.js
+++ j-02.js
@@ -12,7 +12,19 @@ let stringfield =basic.toString()
 console.log(stringfield)
 */
 // 03. Round
+/*
 let num =1.5
 let rounded =Math.round(num)
 
 console.log(rounded)
+*/
+// 04. Arithmétique
+let test =12
+let bis =5
+
+console.log(`test + bis = ${test +bis}`);
+console.log(`test - bis = ${test -bis}`);
+console.log(`text x bis = ${test *bis}`);
+console.log(`text / bis = ${test /bis}`);
+console.log(`text % bis = ${test %bis}`);
+console.log(`text ^ bis = ${test **bis}`);
