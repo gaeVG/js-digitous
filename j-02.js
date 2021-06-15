9bcff96e1699dcabc47c94c69303d688ff564650
Gaev Cenne <gaev@evilparadise.com>
2021-06-15 12:36:12 +0200

    update: exercice 3

--- j-02.js
+++ j-02.js
@@ -5,6 +5,14 @@ let float =13.9
 */
 
 // 02. Convert
+/*
 let basic =34
 let stringfield =basic.toString()
+
 console.log(stringfield)
+*/
+// 03. Round
+let num =1.5
+let rounded =Math.round(num)
+
+console.log(rounded)
